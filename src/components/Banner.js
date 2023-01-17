b7638e7719dfd595ff2ab44fe90d66b2c3637ebb
choiseongyun <5songyon5@naver.com>
2023-01-17 21:40:18 +0900

    잼밌다

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,12 @@
 import axios from '../api/axios';
 import React, { useEffect, useState } from 'react'
 import requests from '../api/requests';
-
+import "./Banner.css";
+import styled from 'styled-components';
 const Banner = () => {
     const [movie, setMovie] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
+    
     useEffect(()=>{
         fetchData();
     },[]);
@@ -25,14 +28,87 @@ const Banner = () => {
         setMovie(movieDetail);
        
     };
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    };
+    console.log(movie);
+if(!isClicked){
+    return (
+        <header
+            className='banner'
+            style={{
+                    backgroundImage : `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                    backgroundPosition: "top center",
+                    backgroundSize: "cover"
+                }}
+        >
+            <div className='banner__contents'>
+                <h1 className='banner__title'> {movie.title || movie.name || movie.original_name} </h1>
+                <div className='banner__buttons'>
+                    <button className='banner__button play' 
+                        onClick={() => setIsClicked(true)}
+                    >
+                    Play</button>
+                    <button className='banner__button info'>More Information</button>
+                </div>
+            
+            <h1 className='banner__description'>
+                {truncate( movie?.overview, 100) }
+            </h1>
 
-return (
-    <div>
-
-        
-    </div>
-  )
-
+            </div>
+            <div className='banner--fadeBottom'/>
+        </header>
+        )
+    }else{
+        return (
+            <Container>
+                <HomeContainer>
+                    <Iframe
+                        width="640"
+                        height="360"
+                        src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?
+                        controls=0&autoplay=1&loop=1mute=1&playlist=${movie.videos.results[0].key}`}
+                        title="판호야 오늘 좀 맞자"
+                        frameBorder="0"
+                        allow="autoplay; fullscreen"
+                        allowFullScreen
+                    >
+                    </Iframe>
+                </HomeContainer>
+            </Container>
+        )
+    }
 }
 
 export default Banner
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction : column;
+    width : 100%;
+    height : 100vh;
+`;
+const HomeContainer = styled.div`
+    width: 100%;
+    height :100%;
+`;
+//대문자로 시작해라
+const Iframe = styled.iframe`
+    width:100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.65;
+    border: none;
+    
+    &::after {
+        content:"";
+        position" absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`;
