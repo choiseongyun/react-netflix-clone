2ed491579bffc687a067592f504daaaf6856fe19
choiseongyun <5songyon5@naver.com>
2023-02-01 23:21:55 +0900

    useDebounce 검색지연시스템

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 export const useDebounce = (value, delay) => {
+  // State and setters for debounced value
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from "../../api/axios";
+import { useDebounce } from '../../hooks/useDebounce';
 import "./SearchPage.css";
 export default function SearchPage() {
         const navigate = useNavigate();
@@ -11,16 +12,18 @@ export default function SearchPage() {
         }
         let query = useQuery();
         const searchTerm = query.get('q');
-        console.log('searchTerm', searchTerm);
+        const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
 
         useEffect(() => {
-            if(searchTerm){
-                fetchSearchMovie(searchTerm);
+            if(debouncedSearchTerm){
+                fetchSearchMovie(debouncedSearchTerm);
 
             }
-        }, [searchTerm]);
+        }, [debouncedSearchTerm]);
         
         const fetchSearchMovie = async (searchTerm) => {
+            console.log('searchTerm',searchTerm);
         try {
             const request = await axios.get(
                 `/search/multi?include_adult=false&query=${searchTerm}`
@@ -42,7 +45,7 @@ export default function SearchPage() {
                     const movieImageUrl =
                     "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
                 return(
-                    <div className='movie'>
+                    <div className='movie' key={movie.id}>
                         <div className='movie__column-poster'>
                             <img src={movieImageUrl} alt="무비무비열매" className='movie__poster' />
             
@@ -56,7 +59,7 @@ export default function SearchPage() {
             <section className="no-results">
                 <div className="no-results__text">
                 <p>
-                    찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.
+                    찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.
                     
                 </p>      
                 </div>
